c2053cfdcbe8e50ae2d8563ef978212ca98c2fb8
deweizhang <1694671322@qq.com>
2020-09-26 17:21:09 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testapp777
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
